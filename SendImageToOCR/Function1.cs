682569a2f060d7fb6bed4c8f21617974f3f767a0
krydge <k.rydge.stubbs@gmail.com>
2019-10-31 14:29:46 -0600

    Fixed ocr function

--- SendImageToOCR/Function1.cs
+++ SendImageToOCR/Function1.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -7,17 +8,25 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Web;
 
 namespace SendImageToOCR
 {
     public static class Function1
     {
+        //static string subscriptionkey = Environment.GetEnvironmentVariable("30f3c8a9825e44b0bc19cbc71141ee78");
+        static string subscriptionkey = "30f3c8a9825e44b0bc19cbc71141ee78";
+        static string endpoint = Environment.GetEnvironmentVariable("https://imagereaderocr.cognitiveservices.azure.com/");
+        // the analyze method endpoint
+        static string uribase = "https://imagereaderocr.cognitiveservices.azure.com/" + "vision/v2.0/describe?";
         [FunctionName("Function1")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-
+            setpath();
             string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
@@ -28,36 +37,61 @@ namespace SendImageToOCR
                 ? (ActionResult)new OkObjectResult($"Hello, {name}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
+        // add your computer vision subscription key and endpoint to your environment variables.
+      
+
+        static void setpath()
+        {
+            // get the path and filename to process from the user.
+            // console.writeline("analyze an image:");
+            // console.write(
+            //"enter the path to the image you wish to analyze: ");
+            string imagefilepath = "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
+
+            if (File.Exists(imagefilepath))
+            {
+                // call the rest api method.
+                // console.writeline("\nwait a moment for the results to appear.\n");
+            }
+            else
+            {
+                MakeAnalysisRequest(imagefilepath);
+                //console.writeline("\ninvalid file path");
+            }
+            //console.writeline("\npress enter to exit...");
+            //console.readline();
+        }
+        static async void MakeAnalysisRequest(string imageFilePath)
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(imageFilePath);
+
+
+            // Request headers. Replace the second parameter with a valid subscription key.
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "30f3c8a9825e44b0bc19cbc71141ee78");
+
+            // Request parameters. You can change "landmarks" to "celebrities" on requestParameters and uri to use the Celebrities model.
+            var uri = uribase+ queryString;
+            
+            Console.WriteLine(uri);
+
+            HttpResponseMessage response;
+
+            // Request body. Try this sample with a locally stored JPEG image.
+            //byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+            byte[] byteData = Encoding.UTF8.GetBytes("{body}");
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                response = await client.PostAsync(uri, content);
+            }
+        }
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
     }
-    //static class Program
-    //{
-    //    // Add your Computer Vision subscription key and endpoint to your environment variables.
-    //    static string subscriptionKey = Environment.GetEnvironmentVariable("COMPUTER_VISION_SUBSCRIPTION_KEY");
-
-    //    static string endpoint = Environment.GetEnvironmentVariable("COMPUTER_VISION_ENDPOINT");
-
-    //    // the Analyze method endpoint
-    //    const string uriBase = endpoint + "vision/v2.1/analyze";
-
-    //    static void Main()
-    //    {
-    //        // Get the path and filename to process from the user.
-    //        Console.WriteLine("Analyze an image:");
-    //        Console.Write(
-    //            "Enter the path to the image you wish to analyze: ");
-    //        string imageFilePath = Console.ReadLine();
-
-    //        if (File.Exists(imageFilePath))
-    //        {
-    //            // Call the REST API method.
-    //            Console.WriteLine("\nWait a moment for the results to appear.\n");
-    //            MakeAnalysisRequest(imageFilePath).Wait();
-    //        }
-    //        else
-    //        {
-    //            Console.WriteLine("\nInvalid file path");
-    //        }
-    //        Console.WriteLine("\nPress Enter to exit...");
-    //        Console.ReadLine();
-    //    }
-    }
+}
--- SendImageToOCR/Services/ImageStuff.cs
+++ SendImageToOCR/Services/ImageStuff.cs
@@ -1,90 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Newtonsoft.Json.Linq;
-using System;
-using System.IO;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using Newtonsoft.Json.Linq;
+//using System;
+//using System.IO;
+//using System.Net.Http;
+//using System.Net.Http.Headers;
+//using System.Threading.Tasks;
 
-namespace SendImageToOCR.Services
-{
-    public class ImageStuff
-    {
-        /// <param name="imageFilePath">The image file to analyze.</param>
-        static async Task MakeAnalysisRequest(string imageFilePath)
-        {
-            try
-            {
-                HttpClient client = new HttpClient();
+//namespace SendImageToOCR.Services
+//{
+//    public class ImageStuff
+//    {
+//        / <param name="imageFilePath">The image file to analyze.</param>
+//        static async Task MakeAnalysisRequest(string imageFilePath)
+//        {
+//            try
+//            {
+//                HttpClient client = new HttpClient();
 
-                // Request headers.
-                client.DefaultRequestHeaders.Add(
-                    "Ocp-Apim-Subscription-Key", subscriptionKey);
+//                 Request headers.
+//                client.DefaultRequestHeaders.Add(
+//                    "Ocp-Apim-Subscription-Key", subscriptionKey);
 
-                // Request parameters. A third optional parameter is "details".
-                // The Analyze Image method returns information about the following
-                // visual features:
-                // Categories:  categorizes image content according to a
-                //              taxonomy defined in documentation.
-                // Description: describes the image content with a complete
-                //              sentence in supported languages.
-                // Color:       determines the accent color, dominant color, 
-                //              and whether an image is black & white.
-                string requestParameters =
-                    "visualFeatures=Categories,Description,Color";
+//                 Request parameters. A third optional parameter is "details".
+//                 The Analyze Image method returns information about the following
+//                 visual features:
+//                 Categories:  categorizes image content according to a
+//                              taxonomy defined in documentation.
+//                 Description: describes the image content with a complete
+//                              sentence in supported languages.
+//                 Color:       determines the accent color, dominant color,
+//                              and whether an image is black & white.
+//                string requestParameters =
+//                    "visualFeatures=Categories,Description,Color";
 
-                // Assemble the URI for the REST API method.
-                string uri = uriBase + "?" + requestParameters;
+//                 Assemble the URI for the REST API method.
+//                string uri = uriBase + "?" + requestParameters;
 
-                HttpResponseMessage response;
+//                HttpResponseMessage response;
 
-                // Read the contents of the specified local image
-                // into a byte array.
-                byte[] byteData = GetImageAsByteArray(imageFilePath);
+//                 Read the contents of the specified local image
+//                 into a byte array.
+//                byte[] byteData = GetImageAsByteArray(imageFilePath);
 
-                // Add the byte array as an octet stream to the request body.
-                using (ByteArrayContent content = new ByteArrayContent(byteData))
-                {
-                    // This example uses the "application/octet-stream" content type.
-                    // The other content types you can use are "application/json"
-                    // and "multipart/form-data".
-                    content.Headers.ContentType =
-                        new MediaTypeHeaderValue("application/octet-stream");
+//                 Add the byte array as an octet stream to the request body.
+//                using (ByteArrayContent content = new ByteArrayContent(byteData))
+//                {
+//                     This example uses the "application/octet-stream" content type.
+//                     The other content types you can use are "application/json"
+//                     and "multipart/form-data".
+//                    content.Headers.ContentType =
+//                        new MediaTypeHeaderValue("application/octet-stream");
 
-                    // Asynchronously call the REST API method.
-                    response = await client.PostAsync(uri, content);
-                }
+//                     Asynchronously call the REST API method.
+//                    response = await client.PostAsync(uri, content);
+//                }
 
-                // Asynchronously get the JSON response.
-                string contentString = await response.Content.ReadAsStringAsync();
+//                 Asynchronously get the JSON response.
+//                string contentString = await response.Content.ReadAsStringAsync();
 
-                // Display the JSON response.
-                Console.WriteLine("\nResponse:\n\n{0}\n",
-                    JToken.Parse(contentString).ToString());
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("\n" + e.Message);
-            }
-        }
+//                 Display the JSON response.
+//                Console.WriteLine("\nResponse:\n\n{0}\n",
+//                    JToken.Parse(contentString).ToString());
+//            }
+//            catch (Exception e)
+//            {
+//                Console.WriteLine("\n" + e.Message);
+//            }
+//        }
 
-        /// <summary>
-        /// Returns the contents of the specified file as a byte array.
-        /// </summary>
-        /// <param name="imageFilePath">The image file to read.</param>
-        /// <returns>The byte array of the image data.</returns>
-        static byte[] GetImageAsByteArray(string imageFilePath)
-        {
-            // Open a read-only file stream for the specified file.
-            using (FileStream fileStream =
-                new FileStream(imageFilePath, FileMode.Open, FileAccess.Read))
-            {
-                // Read the file's contents into a byte array.
-                BinaryReader binaryReader = new BinaryReader(fileStream);
-                return binaryReader.ReadBytes((int)fileStream.Length);
-            }
-        }
-    }
-}
+//        / <summary>
+//        / Returns the contents of the specified file as a byte array.
+//        / </summary>
+//        / <param name="imageFilePath">The image file to read.</param>
+//        / <returns>The byte array of the image data.</returns>
+//        static byte[] GetImageAsByteArray(string imageFilePath)
+//        {
+//             Open a read-only file stream for the specified file.
+//            using (FileStream fileStream =
+//                new FileStream(imageFilePath, FileMode.Open, FileAccess.Read))
+//            {
+//                 Read the file's contents into a byte array.
+//                BinaryReader binaryReader = new BinaryReader(fileStream);
+//                return binaryReader.ReadBytes((int)fileStream.Length);
+//            }
+//        }
+//    }
+//}
